196084fb98051600dd3e4d7b9a3a3bd2463fee7d
Alex Sanchez <alexander.sanchez.ps@live.com>
2021-08-16 22:35:59 -0400

    Only users who are logged in are admitted to the pages and users cannot access other users portals

--- accounts/views.py
+++ accounts/views.py
@@ -33,7 +33,7 @@ def login_view(request):
     return render (request,"registration/login.html", {"form":form})
 def logout_view(request):
     logout(request)
-    return redirect("/login")
+    return redirect("login")
 def register_view(request):
     form = RegisterForm(request.POST or None)
     if form.is_valid():
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# @login_required(login_url='login')
 def customer_view(request):
-    return render(request,"profiles/customer.html")
+    current_user = request.user
+    if current_user.is_authenticated:
+        if not current_user.is_customer:
+            messages.error(request,"You do not have access to this page")
+            return redirect('login')
+        return render(request,"profiles/customer.html")
+    else:
+        messages.error(request,"You need to be logged in to access this page")
+        return redirect('login')
 def staff_view(request):
-    return render(request,"profiles/staff.html")
+    current_user = request.user
+    if current_user.is_authenticated:
+        if not current_user.is_staff:
+            messages.error(request,"You do not have access to this page")
+            return redirect('login')
+        return render(request,"profiles/staff.html")
+    else:
+        messages.error(request,"You need to be logged in to access this page")
+        return redirect('login')
 def admin_view(request):
-    return render(request,"profiles/admin.html")
+    current_user = request.user
+    if current_user.is_authenticated:
+        if not current_user.is_superuser:
+            messages.error(request,"You do not have access to this page")
+            return redirect('login')
+        return render(request,"profiles/administration.html")
+    else:
+        messages.error(request,"You need to be logged in to access this page")
+        return redirect('login')
