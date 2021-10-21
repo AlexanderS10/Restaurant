564bd6791aaea53cd2595b8795937be1d53b5137
Alexander Sanchez <57728893+AlexanderS10@users.noreply.github.com>
2021-10-21 14:29:33 -0400

    Added a layout for the profile and set the fonts back to default

--- static/js/customer/customer_home.js
+++ static/js/customer/customer_home.js
@@ -14,48 +14,29 @@ function getCookie(name) {
     return cookieValue;
   }
 const csrftoken = getCookie('csrftoken');
-console.log("This is the token: "+csrftoken)
-//Use of the API as excercise even though I could retrieve the data from the session.
-//xhr requests are aynchronous meaning that they will execute parallel to the rest of the code and therefore not stop other code form executing.
-//Therefore, all actions with the request must be done inside the onreadystate or onload
-const xhr = new XMLHttpRequest();
-const method = 'GET';
-const url = "api/userdetails";
-const responseType = "json";
-let user_data = null;
-xhr.responseType = responseType; // Let the xhr request know that its getting a json 
-xhr.open(method, url, true); //This opens the request with the method and url entered
-xhr.onload = function(){//onload is a property that is available on mostly modern browsers 
-    if(xhr.status!=200){
+// console.log("This is the token: "+csrftoken)
 
-    }else{
-        console.log(xhr.response)
-    }
-    
-}
-xhr.send(null);//Trigger that request
+/*
+/NAVBAR FUNCTIONALITY
+*/
 const navbarToggle = navbar.querySelector("#navbar-toggle");
 const navbarMenu = document.querySelector("#navbar-menu");
 const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
 let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
-
 const toggleNavbarVisibility = () => {
   isNavbarExpanded = !isNavbarExpanded;
   navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
 };
-
 navbarToggle.addEventListener("click", toggleNavbarVisibility);
-
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
 
+/*
+/Catch and send the calendar date to the backend
+*/
 //Grab the element in the p tag
 let form = document.getElementById("form-date");
 let form2 = document.getElementById("form-date2");
-//console.log(String(inputTemp));
-//console.log("This is the date: "+date);
-
-
 function setDate() {  
     let date = document.getElementById('date_picked').textContent;
     document.getElementById("form-date").action = "book-event/"+String(date)+"/"
--- static/js/customer/profile.js
+++ static/js/customer/profile.js
@@ -1,7 +1,5 @@
 /*
-/
-/NaAVBAR FUNCTIONALITY
-/
+/NAVBAR FUNCTIONALITY
 */
 const navbarToggle = navbar.querySelector("#navbar-toggle");
 const navbarMenu = document.querySelector("#navbar-menu");
@@ -17,3 +15,22 @@ navbarToggle.addEventListener("click", toggleNavbarVisibility);
 
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+/*
+/API USE
+*/
+const xhr = new XMLHttpRequest();
+const method = 'GET';
+const url = "../api/userdetails";
+const responseType = "json";
+let user_data = null;
+xhr.responseType = responseType; // Let the xhr request know that its getting a json 
+xhr.open(method, url, true); //This opens the request with the method and url entered
+xhr.onload = function(){//onload is a property that is available on mostly modern browsers 
+    if(xhr.status!=200){
+
+    }else{
+        console.log(xhr.response)
+    }
+}
+xhr.send(null);//Trigger that request
